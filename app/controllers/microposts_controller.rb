934f9d68bc0228028663380f84d5986e5232d67d
gousuke <your.tf.ekdn@gmail.com>
2018-05-17 22:09:05 +0900

    なんやこのスペース消したよ

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -59,7 +59,6 @@ class MicropostsController < ApplicationController
     redirect_to request.referrer || root_url
   end
 
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_micropost
